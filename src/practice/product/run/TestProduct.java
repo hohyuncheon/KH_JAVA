8d09f28fd15bcbaed0587fc29a3ece45396d0b5b
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2020-12-14 15:03:24 +0900

    Create TestProduct.java

---
+++ src/practice/product/run/TestProduct.java
@@ -0,0 +1,15 @@
+package product.run;
+
+import product.model.vo.Product;
+
+public class TestProduct {
+
+	public static void main(String []args) {
+		
+		Product pd = new Product();
+		pd.printProduct();
+
+		
+		
+	}
+}
