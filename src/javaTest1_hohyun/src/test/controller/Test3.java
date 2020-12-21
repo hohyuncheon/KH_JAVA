a46b60f23752b94b9e90a1bad2a29ffedf50cb74
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2020-12-22 01:37:01 +0900

    Create Test3.java

---
+++ src/javaTest1_hohyun/src/test/controller/Test3.java
@@ -0,0 +1,32 @@
+package test.controller;
+
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		
+		double t=0;
+		
+		int i=1;
+		while(i<=100) {
+			t += i;
+			i++;
+			} 
+		double total = t;
+		
+		
+		double ave = total / i;
+		
+		System.out.println("합계"+total +
+							"\n평균"+ave );
+
+		
+		
+			
+		
+
+		
+	}
+
+}
