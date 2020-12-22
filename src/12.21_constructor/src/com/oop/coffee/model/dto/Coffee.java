20b6c3ca3e9cb4d5af0febc7c503b903ca2367ae
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2020-12-23 02:07:59 +0900

    Create Coffee.java

---
+++ src/12.21_constructor/src/com/oop/coffee/model/dto/Coffee.java
@@ -0,0 +1,45 @@
+package com.oop.coffee.model.dto;
+
+/**
+ * 
+ * Coffee 멤버변수 원산지 origin,
+ * 지역 location 기본생성자,
+ * 모든필드초기화생성자 getter,
+ * setter 출력메소드
+ *
+ */
+
+public class Coffee {
+
+	private String origin;
+	private String location;
+
+	public void print() {
+		System.out.println(origin + "         " + location);
+	}
+
+	public Coffee() {
+	}
+
+	public Coffee(String origin, String location) {
+		this.origin = origin;
+		this.location = location;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
