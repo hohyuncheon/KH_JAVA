afcd3cc7566e9a259b4b0226927ae64ed26b38d0
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2020-12-23 02:08:02 +0900

    Create Run.java

---
+++ src/12.21_constructor/src/com/oop/coffee/run/Run.java
@@ -0,0 +1,9 @@
+package com.oop.coffee.run;
+
+import com.oop.coffee.controller.CoffeeManager;
+
+public class Run {
+	public static void main(String[] args) {
+		CoffeeManager coffeeManager = new CoffeeManager();
+	}
+}
