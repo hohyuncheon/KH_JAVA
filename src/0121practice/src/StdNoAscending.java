b5dc530ef3548c00af2ddd86969906cb323b6c8d
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2021-01-22 01:38:14 +0900

    0121practice

---
+++ src/0121practice/src/StdNoAscending.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class StdNoAscending implements Comparator<Student>{
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getStdNo()-o2.getStdNo();
+    }
+
+}
