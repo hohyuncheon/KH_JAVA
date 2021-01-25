758f048ec7d4804e7de4fd00d578e1022bf356f6
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2021-01-25 14:19:18 +0900

    re

--- src/test/12.21_constructor/src/com/oop/coffee/run/Run.java
+++
@@ -1,9 +0,0 @@
-package com.oop.coffee.run;
-
-import com.oop.coffee.controller.CoffeeManager;
-
-public class Run {
-	public static void main(String[] args) {
-		CoffeeManager coffeeManager = new CoffeeManager();
-	}
-}
