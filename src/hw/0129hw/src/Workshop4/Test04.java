f26fa2d702257bd4766545b52b8749363cd5e468
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2021-01-29 03:06:28 +0900

    0129array

---
+++ src/hw/0129hw/src/Workshop4/Test04.java
@@ -0,0 +1,17 @@
+package Workshop4;
+//프로그램 실행 시 eclipse argument로 5~10까지의 정수형 데이터를 입력 받는다.
+//입력 받은 정수 값을 Calc class의 calculate()함수를 이용하여 1부터 입력 받은 숫자까지 짝수만 더하는 프로그램을 작성 한다.
+
+public class Test04 {
+	public static void main(String[] args) {
+		
+		Calc c = new Calc();
+		
+		int ii =Integer.parseInt(args[0]);
+		
+		System.out.println("입력값 :" + ii);
+		System.out.println("합계는 :"+c.calculate(ii));
+	}
+	
+
+}
---
+++ src/hw/0129hw/src/Workshop4/Test05.java
@@ -0,0 +1,34 @@
+package Workshop4;
+
+//eclipse에서 argument로 1~5까지의 정수형 데이터 하나를 입력 받아 입력 받은 수부터 10까지 합을 구한다.
+//단, 3의 배수와 5의 배수는 합에서 제외 한다.
+//3. 실행 결과
+//출력은 아래와 같이 이루어 진다.5을 입력 할 경우
+//7+8
+//결과: 15
+
+public class Test05 {
+
+	public static void main(String[] args) {
+
+		// args값
+		int num = Integer.parseInt(args[0]);
+
+		int sum = 0;
+
+		for (int i = num; i < 10; i++) {
+
+			if (!(i % 5 == 0 || i % 3 == 0)) {
+				if (sum==0) {
+					System.out.print(i);
+					
+				}else
+					System.out.print(" + " +i);
+				sum += i;
+			}
+		}
+		System.out.println();
+		System.out.println("합계" + sum);
+	}
+
+}
