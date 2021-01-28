8361c499fe937261c146c1936e16fc14dbab9f74
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2021-01-29 03:06:17 +0900

    0129array

---
+++ src/hw/0129hw/src/Workshop4/Calc.java
@@ -0,0 +1,19 @@
+package Workshop4;
+
+public class Calc {
+
+	public int calculate(int intt) {
+
+		int sum = 0;
+		
+		System.out.print("짝수 : ");
+		for (int i = 1; i < intt; i++) {
+			if(i%2==0) {
+				sum += i;
+				System.out.print(i+" ");
+			}
+		}
+		System.out.println();
+		return sum;
+	}
+}
